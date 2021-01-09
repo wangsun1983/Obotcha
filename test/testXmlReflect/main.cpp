f6037b6e3fb5e7d0fd20698d99f9472ea934fc2b
sunli.wang <wang_sun_1983@yahoo.co.jp>
2021-01-09 19:52:04 +0800

    add xml reflect unit test

--- test/testXmlReflect/main.cpp
+++ test/testXmlReflect/main.cpp
@@ -1,25 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <iostream>
-#include <functional>
 
-#include "Thread.hpp"
-#include "Runnable.hpp"
-#include "BlockingQueue.hpp"
-#include "ExecutorService.hpp"
-#include "Integer.hpp"
-#include "String.hpp"
-#include "ArrayList.hpp"
-#include "Reflect.hpp"
-#include "Integer.hpp"
-#include "JsonReader.hpp"
-#include <vector>
-
-using namespace obotcha;
-
-extern void testImportFrom();
+extern int testReflectToSimpleXml();
+extern int testReflectToArrayXml();
+extern int testReflectToSimpleArrayXml();
+extern int testReflectNumber();
 
 int main() {
-    //testReflect();
-    testImportFrom();
+  testReflectNumber();
+  testReflectToArrayXml();
+  testReflectToSimpleArrayXml();
+  testReflectToSimpleXml();
 }
